4866a6eef527b4104eb7cdc5ffdfd008329f62b7
n644dc <n644dc@gmail.com>
2017-12-09 18:31:18 -0700

    saqdfasdf

--- account_generator.py
+++ account_generator.py
@@ -6,13 +6,17 @@ import os
 class WalletGen:
     def __init__(self):
 
-        isLinux = sys.platform.lower().startswith('linux')
+        self.isLinux = sys.platform.lower().startswith('linux')
 
         # Init Vars
-        self.workDir = '/var/www/html/bitcon' if isLinux else 'C:\\bitcon'
-        self.phraseLocation = "{}/phrases/".format(self.workDir) if isLinux else "{}\\phrases\\".format(self.workDir)
+        self.workDir = '/var/www/html/bitcon' if self.isLinux else 'C:\\bitcon'
+        self.walletsLoc = "{}/gen/".format(self.workDir) if self.isLinux else "{}\\gen\\".format(self.workDir)
         self.wordFile = 'linux.words'
-        self.Phrases = []
+        self.walletsTotalCount = 0
+        self.lastWalletCount = 1
+        self.walFileSize = 1000
+        self.walFolderSize = 20000
+
         self.Wallets = []
         self.words = []
 
@@ -20,21 +24,69 @@ class WalletGen:
         if not os.path.exists(self.workDir):
             os.makedirs(self.workDir)
 
-        if not os.path.exists(self.phraseLocation):
-            os.makedirs(self.phraseLocation)
+        if not os.path.exists(self.walletsLoc):
+            os.makedirs(self.walletsLoc)
+
+        self.currentWalletFolder = "{}{}_{}".format(self.walletsLoc, self.lastWalletCount, self.walFolderSize)
+
+
+    @staticmethod
+    def representsInt(s):
+        try:
+            int(s)
+            return True
+        except ValueError:
+            return False
+
+    def saveWallets(self):
+        currentNumOfWallets = len(self.Wallets)
+        if currentNumOfWallets == self.walFileSize:
+            fileNom = "{}_{}.txt".format((self.walletsTotalCount-currentNumOfWallets)+1, self.walletsTotalCount)
+            walletFile = "{}/{}".format(self.currentWalletFolder,fileNom) if self.isLinux else "{}\\{}".format(self.currentWalletFolder,fileNom)
+
+            if not os.path.exists(self.currentWalletFolder):
+                os.makedirs(self.currentWalletFolder)
+
+            with open(walletFile, "w") as savFile:
+                for wallet in self.Wallets:
+                    line = "{}\n".format(", ".join(wallet))
+                    savFile.write(line)
+
+            self.Wallets = []
+
+            if self.walletsTotalCount % self.walFolderSize == 0:
+                self.lastWalletCount = self.walletsTotalCount + 1
+                walletFolderName = "{}_{}".format(self.lastWalletCount, self.walletsTotalCount + self.walFolderSize)
+                self.currentWalletFolder = "{}{}".format(self.walletsLoc, walletFolderName)
+
+
+
+    def genPrep(self, phraseArray):
+        # Prase with spaces
+        phrase = " ".join(phraseArray)
+        self.generateWallet(phrase)
+
+        # Phrase smooshed together if last word is integer
+        if len(phraseArray) > 1 and self.representsInt(phraseArray[2]):
+            phrase = "".join(phraseArray)
+            self.generateWallet(phrase)
 
 
     def generateWallet(self, phrase):
-        # Save Phrase
-        self.Phrases.append(phrase)
+        private_key = BitcoinPrivateKey.from_passphrase(phrase)
+        phrase = private_key.passphrase()
+        privateHex = private_key.to_hex()
+        privateWIF = private_key.to_wif()
 
-        private_key = BitcoinPrivateKey.from_passphrase("AA")
-        print(private_key.passphrase())
-        private_key.to_hex()
-        priv2 = BitcoinPrivateKey.from_passphrase(private_key.passphrase())
-        areSame = private_key.to_hex() == priv2.to_hex()
-        print(areSame)
+        public_key = private_key.public_key()
+        publicHex  = public_key.to_hex()
+        publicAddr = public_key.address()
+        publicH160 = public_key.hash160()
 
+        wallet = [phrase, privateHex, privateWIF, publicHex, publicAddr, publicH160]
+        self.Wallets.append(wallet)
+        self.walletsTotalCount += 1
+        self.saveWallets()
 
     def generatePhrases(self):
         with open(self.wordFile) as f:
@@ -43,8 +95,11 @@ class WalletGen:
         self.words = [x.strip() for x in self.words]
 
         for word in self.words:
-            phrase = [].append(word)
-            self.generateWallet(phrase)
+            self.genPrep([word])
+
+            for word2 in self.words:
+                self.genPrep([word, word2])
+
 
 
 def main():
